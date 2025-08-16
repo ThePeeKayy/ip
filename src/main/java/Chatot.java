f0fb4168c8633a6805f979590201529ef489d529
pengkiang <pengkiangtan@gmail.com>
2025-08-16 16:58:29 +0800

    Renaming chat application to Chatot in files

---
+++ src/main/java/Chatot.java
@@ -0,0 +1,10 @@
+public class Chatot {
+    public static void main(String[] args) {
+        String logo = "  ____ |  |__ _____ _/  |_  _____/  |_ \n"
+                + "_/ ___\\|  |  \\\\__  \\\\   __\\/  _ \\   __\\\n"
+                + "\\  \\___|   Y  \\/ __ \\|  | (  <_> )  |  \n"
+                + " \\___  >___|  (____  /__|  \\____/|__|  \n"
+                + "     \\/     \\/     \\/                  \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
