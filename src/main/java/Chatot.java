ad0faf44fd5864a945a243c7f546e74cdca0c19b
pengkiang <pengkiangtan@gmail.com>
2025-08-16 17:32:46 +0800

    Addition of skeletal structure for greet and exit

--- src/main/java/Chatot.java
+++ src/main/java/Chatot.java
@@ -1,4 +1,14 @@
 public class Chatot {
+    private final String userCommand;
+
+    public Chatot() {
+        this.userCommand = "";
+    }
+
+    public Chatot(String inputCommand) {
+        this.userCommand = inputCommand;
+    }
+
     public static void main(String[] args) {
         String logo = "  ____ |  |__ _____ _/  |_  _____/  |_ \n"
                 + "_/ ___\\|  |  \\\\__  \\\\   __\\/  _ \\   __\\\n"
@@ -6,5 +16,24 @@ public class Chatot {
                 + " \\___  >___|  (____  /__|  \\____/|__|  \n"
                 + "     \\/     \\/     \\/                  \n";
         System.out.println("Hello from\n" + logo);
+
+        if (userCommand.isEmpty()) {
+            greet();
+        }
+
+        exit();
     }
+
+    public static void greet() {
+        System.out.println("Hello I'm Chatot!");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+//
+//    public static void echo(String inputCommand) {
+//        System.out.println(inputCommand);
+//    }
 }
