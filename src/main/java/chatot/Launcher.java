4572ef3a5f45e425a3ed3c6e0ef1a346ee0680b1
pengkiang <pengkiangtan@gmail.com>
2025-08-26 00:41:46 +0800

    Add basic chat interface

---
+++ src/main/java/chatot/Launcher.java
@@ -0,0 +1,9 @@
+package chatot;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Chatot.class, args);
+    }
+}
